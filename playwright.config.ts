15d0f6ff40e9c726fb76352c1440e43930a62361
Vaibhav Gautam <65037939+vaibhavGautam763@users.noreply.github.com>
2025-07-06 11:06:11 +0530

    Delete playwright.config.ts

--- playwright.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@playwright/test';
-
-export default defineConfig({
-  reporter: [['html', { open: 'never' }]], // HTML report banega
-  use: {
-    screenshot: 'only-on-failure',        // Sirf fail par screenshot
-    video: 'retain-on-failure'            // Sirf fail par video
-  },
-});
